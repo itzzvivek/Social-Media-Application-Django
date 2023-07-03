9012f6c587cf9dd9595a68d31e6f43f9dc017aa5
itzzvivek <vivekwarkade000@gmail.com>
2023-07-03 20:02:24 +0530

    Models are Created

--- core/models.py
+++ core/models.py
@@ -14,3 +14,28 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+    
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
+class FollowerCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
--- core/views.py
+++ core/views.py
@@ -18,21 +18,21 @@ def setting(request):
     if request.method == 'POST':
 
         if request.FILES.get('image') == None:
-            image = user_profile.profile_image
+            image = user_profile.profileimg
             bio = request.POST['bio']
             location = request.POST['location']
 
-            user_profile.profile_img = image
+            user_profile.profileimg = image
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
             
         if request.FILES.get('image') != None:
-            image = user_profile.profile_image
+            image = request.FILES.get('image')
             bio = request.POST['bio']
             location = request.POST['location']
 
-            user_profile.profile_img = image
+            user_profile.profileimg = image    
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
