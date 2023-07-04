7995d07ab1628e784249a22c688b094d869edd33
itzzvivek <vivekwarkade000@gmail.com>
2023-07-04 22:29:55 +0530

    issues in upload

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile,Post,FollowerCount,LikePost
 
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(FollowerCount)
+admin.site.register(LikePost)
