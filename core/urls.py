6ccba833633d080ae6b604a9df77f90121f7129c
itzzvivek <vivekwarkade000@gmail.com>
2023-07-11 16:28:47 +0530

    profile page

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('setting/', views.setting, name='setting'),
     path('upload/', views.upload, name='upload'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post/', views.like_post, name='like-post'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
