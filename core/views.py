56050a08267792d2dccd254e9d6c1f167989e868
itzzvivek <vivekwarkade000@gmail.com>
2023-07-14 23:15:17 +0530

    changes

--- core/views.py
+++ core/views.py
@@ -22,11 +22,25 @@ def profile(request,pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowerCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowerCount.objects.filter(user=pk))
+    user_following = len(FollowerCount.objects.filter(user=pk))
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts' : user_posts,
-        'user_post_length' : user_post_length
+        'user_post_length' : user_post_length,
+        'button_text' : button_text,
+        'user_followers' : user_followers,
+        'user_following' : user_following
         }
     return render(request,'profile.html', context)
 
