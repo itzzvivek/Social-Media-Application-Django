c1481edbf6ec8199edc495fbd1def8457417b27f
itzzvivek <vivekwarkade000@gmail.com>
2023-07-14 22:43:34 +0530

    issue follow

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('setting/', views.setting, name='setting'),
     path('upload/', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post/', views.like_post, name='like-post'),
     path('signup/', views.signup, name='signup'),
